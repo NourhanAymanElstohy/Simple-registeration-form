f27d9ae6bc12a0cd86c33cc9ed83e15a7cf42ce0
nour <nour7anelsto7y@gmail.com>
2020-07-02 23:25:15 +0200

    app index

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <h1>Welcome to My app</h1>
+        </div>
+        <div class="row justify-content-center">
+            <?php
+            if (!isset($_SESSION["email"]) || !isset($_SESSION['login']) || !isset($_SESSION['register'])) { ?>
+                <p>Please <a href="register.php" class="text-primary">Register</a> Or <a href="login.php">Log In</a> If you already have an account</p>
+            <?php
+            } else { ?>
+                <p class="text-success">Thank you for Your login</p>
+                <p><a href="logout.php">Log Out</a></p>
+            <?php } ?>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+</body>
+
+</html>
