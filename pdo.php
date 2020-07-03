967454ed98b62c0248b0e2792a2fadcc13fba5ca
nour <nour7anelsto7y@gmail.com>
2020-07-03 19:47:03 +0200

    fix db errors

--- pdo.php
+++ pdo.php
@@ -2,12 +2,12 @@
 require_once "config.php";
 
 // ================ development ==========
-$host = HOST;
-$port = PORT;
-$DB = DATABASE;
+// $host = HOST;
+// $port = PORT;
+// $DB = DATABASE;
 
-$db = new PDO("mysql:host=$host; port=$port; dbname=$DB", USER, PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $db = new PDO("mysql:host=$host; port=$port; dbname=$DB", USER, PASSWORD);
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //============ production ===============
 $pg_dsn = "pgsql:host=" . $pg_host . ";port=" . $pg_port . ";dbname=" . $pg_db;
 $pg_db = new PDO($pg_dsn, $pg_user, $pg_password);
