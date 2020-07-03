c141074b6ae15f550d1530a70c6cc386b0715567
nour <nour7anelsto7y@gmail.com>
2020-07-03 20:11:53 +0200

    define environments

--- pdo.php
+++ pdo.php
@@ -1,14 +1,27 @@
 <?php
 require_once "config.php";
 
-// ================ development ==========
-// $host = HOST;
-// $port = PORT;
-// $DB = DATABASE;
+$_ENV['development'] = "dev";
+$_ENV['production'] = "prod";
 
-// $db = new PDO("mysql:host=$host; port=$port; dbname=$DB", USER, PASSWORD);
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if ($_SERVER["HTTP_HOST"] == "localhost") {
+    unset($_ENV['production']);
+} else {
+    unset($_ENV['development']);
+}
+die($_SERVER["HTTP_HOST"]);
 //============ production ===============
-$pg_dsn = "pgsql:host=" . $pg_host . ";port=" . $pg_port . ";dbname=" . $pg_db;
-$db = new PDO($pg_dsn, $pg_user, $pg_password);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if (isset($_ENV['production'])) {
+    $pg_dsn = "pgsql:host=" . $pg_host . ";port=" . $pg_port . ";dbname=" . $pg_db;
+    $db = new PDO($pg_dsn, $pg_user, $pg_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} elseif (isset($_ENV['development'])) {
+
+    // ================ development ==========
+    $host = HOST;
+    $port = PORT;
+    $DB = DATABASE;
+
+    $db = new PDO("mysql:host=$host; port=$port; dbname=$DB", USER, PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
