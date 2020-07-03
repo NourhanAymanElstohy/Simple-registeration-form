24b9657ee1bb9699e41ba8800ccc5b269a508fb8
nour <nour7anelsto7y@gmail.com>
2020-07-03 19:16:29 +0200

    connect to pgsql on heroku

--- pdo.php
+++ pdo.php
@@ -1,7 +1,16 @@
 <?php
-require_once "config.php";
-$host = HOST;
-$port = PORT;
-$DB = DATABASE;
-$db = new PDO("mysql:host=$host; port=$port; dbname=$DB", USER, PASSWORD);
+// require_once "config.php";
+// $host = HOST;
+// $port = PORT;
+// $DB = DATABASE;
+//======================
+$pg_host = "ec2-107-22-7-9.compute-1.amazonaws.com";
+$pg_db = "df9328haf3ikkm host=ec2-107-22-7-9.compute-1.amazonaws.com";
+$pg_user = "wfyekatjhpordw";
+$pg_password = "09d1295dae089785ba8ad59e6c5de2ebaf548d383dd002796464a1b0405bdbec";
+$pg_port = "5432";
+
+$pg_dsn = "pgsql:host=" . $pg_host . ";port=" . $pg_port . ";dbname=" . $pg_db;
+$db = new PDO($pg_dsn, $pg_user, $pg_password);
+// $db = new PDO("mysql:host=$host; port=$port; dbname=$DB", USER, PASSWORD);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
